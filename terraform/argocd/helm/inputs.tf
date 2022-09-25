60dd258b068062f09f49b5d824631e4863304bde
Mateusz Kurowski <pythoncontrol@gmail.com>
2022-09-25 22:46:46 +0200

    Update inputs.tf

--- terraform/argocd/helm/inputs.tf
+++ terraform/argocd/helm/inputs.tf
@@ -3,14 +3,3 @@
 variable "cluster_state" {
   default = "../../cluster/digitalocean/terraform.tfstate"
 }
-
-data "terraform_remote_state" "cluster" {
-  backend = "local"
-  config = {
-    path = var.cluster_state
-  }
-}
-
-locals {
-  cluster = data.terraform_remote_state.cluster.outputs.kube_config
-}
